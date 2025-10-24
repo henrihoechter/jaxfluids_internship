37a364b9ee706ba4deca6e489dfc325f6741fafe
hhoechter <code.henri@hoechter.net>
2025-10-24 19:07:29 +0000

    diagnosis

---
+++ src/compressible_1d/diagnose_test.py
@@ -0,0 +1,30 @@
+import jax.numpy as jnp
+import pytest
+
+from src.compressible_1d import diagnose
+
+
+def test_check_nonnegativity_success():
+    U_test = jnp.ones((3, 10))
+
+    diagnose.check_nonnegativity(U_test)
+
+
+def test_check_nonnegativity_with_wrong_mass():
+    """Tests if wrong mass is caught correctly."""
+    U_test = jnp.concatenate(
+        [jnp.stack([-1.0, 0.0, 0.0], axis=0)[:, jnp.newaxis], jnp.ones((3, 10))],
+        axis=-1,
+    )
+    with pytest.raises(ValueError, match="Mass"):
+        diagnose.check_nonnegativity(U_test)
+
+
+def test_check_nonnegativity_with_wrong_energy():
+    """Tests if wrong mass is caught correctly."""
+    U_test = jnp.concatenate(
+        [jnp.stack([0.0, 0.0, -1.0], axis=0)[:, jnp.newaxis], jnp.ones((3, 10))],
+        axis=-1,
+    )
+    with pytest.raises(ValueError, match="Energy"):
+        diagnose.check_nonnegativity(U_test)
