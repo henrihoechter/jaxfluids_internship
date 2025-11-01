49657c4aa37cf8b9a254ec6e95dd941066c54989
hhoechter <code.henri@hoechter.net>
2025-11-01 18:09:27 +0000

    run function

--- src/compressible_1d/numerics.py
+++ src/compressible_1d/numerics.py
@@ -1,6 +1,22 @@
 from jaxtyping import Float, Array
+import jax.numpy as jnp
+import jax
+from dataclasses import dataclass
 
-from compressible_1d import boundary_conditions, solver, diagnose
+from compressible_1d import boundary_conditions, solver, diagnose, numerics
+
+@dataclass
+class Input:
+    U_init: Float[Array, "3 n_cells"]
+    gamma: float
+    boundary_condition:str
+    solver_type:str 
+    delta_x: float
+    delta_t: float
+    n_steps: int
+    n_ghost_cells: int
+    is_debug: bool
+    is_abort: bool
 
 
 def step(
@@ -36,6 +52,43 @@ def step(
     dU_dt = -1 / delta_x * (flux_R - flux_L)
 
     return U_field + delta_t * dU_dt
+
+
+def run(input: Input):
+    U_field = input.U_init
+
+    step = jax.jit(numerics.step, static_argnames=('n_ghost_cells','boundary_condition_type', 'solver_type'))
+
+    U_solutions = jnp.empty((input.U_init.shape[0],input.U_init.shape[1], input.n_steps))
+
+    U_solutions = U_solutions.at[:, :, 0].set(input.U_init)
+    for i in range(input.n_steps):
+        if input.is_debug:
+            print(f"step: {i}")
+
+        new_U_field = step(
+            U_field,
+            input.n_ghost_cells,
+            input.delta_x,
+            input.delta_t,
+            input.gamma,
+            boundary_condition_type=input.boundary_condition,
+            solver_type=input.solver_type,
+        )
+        # diagnose solution
+        diagnose.check_all(
+            new_U_field, U_field, debug=input.is_debug, abort=input.is_abort
+        )
+
+        if i % 100 == 0:
+            diagnose.live_diagnostics(new_U_field, i)
+
+        U_solutions = U_solutions.at[:, :, i].set(new_U_field)
+        U_field = new_U_field
+
+    return U_solutions
+
+
 def calculate_dt(
     U: Float[Array, "3 N"], gamma: float, delta_x: float, cmax: float = 1.0
 ) -> float:
